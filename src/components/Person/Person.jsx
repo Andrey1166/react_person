fc372c0dfb8f00e4d2cd879f5950d9e294cf0e66
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-27 04:23:33 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,6 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partnerLabel =
-    sex === 'm'
-      ? 'wife'
-      : 'husband';
+  const partnerLabel = sex === 'm' ? 'wife' : 'husband';
   const partnerText =
     isMarried === true
       ? `${partnerName} is my ${partnerLabel}`
@@ -12,7 +9,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
